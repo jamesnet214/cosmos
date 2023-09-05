ddb20172387bdaac8a8c8d938d1b5a96dc5441c5
JAEWUNG LEE <james@jamesnet.dev>
2023-09-05 21:02:22 +0900

    Updates

---
+++ src/Cosmos/Cosmos.Main/CustomControl1.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Cosmos.Main
+{
+    public class MyCustomControl : Control
+    {
+        public static readonly StyledProperty<IBrush?> BackgroundProperty =
+            Border.BackgroundProperty.AddOwner<MyCustomControl>();
+
+        public IBrush? Background
+        {
+            get { return GetValue(BackgroundProperty); }
+            set { SetValue(BackgroundProperty, value); }
+        }
+
+        public sealed override void Render(DrawingContext context)
+        {
+            if (Background != null)
+            {
+                var renderSize = Bounds.Size;
+                context.FillRectangle(Background, new Rect(renderSize));
+            }
+            base.Render(context);
+        }
+    }
+}
---
+++ src/Cosmos/Cosmos/Cosmos/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Cosmos.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
